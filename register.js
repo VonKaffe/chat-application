1e3d1900572199ee2efa0f511dd4913be3fc9847
Simon Appeltofft <simonappeltofft@hotmail.com>
2018-02-20 16:30:39 +0100

    Added a js file for register and change ids on html

---
+++ register.js
@@ -0,0 +1,27 @@
+var name = document.getElementById('name');
+var password = document.getElementById('password');
+
+// storing input from register-form
+function store() {
+    localStorage.setItem('name', name.value);
+    localStorage.setItem('password', password.value);
+}
+
+// check if stored data from register-form is equal to entered data in the   login-form
+function check() {
+
+    // stored data from the register-form
+    var storedName = localStorage.getItem('name');
+    var storedPw = localStorage.getItem('password');
+
+    // entered data from the login-form
+    var userName = document.getElementById('userName');
+    var userPw = document.getElementById('userPw');
+
+    // check if stored data from register-form is equal to data from login form
+    if(userName.value !== storedName || userPw.value !== storedPw) {
+        alert('ERROR');
+    }else {
+        alert('You are loged in.');
+    }
+}
